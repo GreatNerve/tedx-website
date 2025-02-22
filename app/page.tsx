d5b8b58eb1484554388217c5365087ab19153a62
apophis04 <kislaygautam04@outlook.com>
2025-02-22 22:05:24 +0530

    corsusel-defined

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,10 @@ import { loadFull } from "tsparticles";
 import { Container, ISourceOptions } from "@tsparticles/engine";
 import { navData, tsParticleOptions } from "@/lib/constants";
 import Link from "next/link";
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {Autoplay} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/autoplay';
 
 const Home = () => {
   const [scrolled, setScrolled] = useState(false);
@@ -140,15 +144,15 @@ const Home = () => {
         </div>
       </div>
 
-        {init && (
-          <Particles
-            id="tsparticles"
-            particlesLoaded={particlesLoaded}
-            options={options}
-            className="fixed -z-10 top-0 left-0 w-full h-full"
-            // className="fixed top-0 left-0 w-full h-full"
-          />
-        )}
+      {init && (
+        <Particles
+          id="tsparticles"
+          particlesLoaded={particlesLoaded}
+          options={options}
+          className="fixed -z-10 top-0 left-0 w-full h-full"
+        // className="fixed top-0 left-0 w-full h-full"
+        />
+      )}
       <div id="about" className="relative bg-black w-full -z-30 px-16 py-7">
         <div className="max-w-[60%] mx-auto mt-7 gap-3">
           <h1 className="text-[7rem] uppercase text-center font-bold flex-2">
@@ -167,6 +171,65 @@ const Home = () => {
               empowers global innovators to amplify their impact.
             </div>
           </div>
+        </div>
+      </div>
+      {/* ------------------Past Events---------------------- */}
+      <div id="events" className="events section-spacing">
+        <h2>Previous Event Highlights</h2>
+        <div className="carouse">
+          <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        loop={true}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+        modules={[Autoplay]}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+      >
+            <SwiperSlide><Image src="/event1.jpg" alt="Event 1" height={50} width={50} /></SwiperSlide>
+            <SwiperSlide><Image src="/event2.jpg" alt="Event 2" height={50} width={50} /></SwiperSlide>
+            <SwiperSlide><Image src="/event3.jpg" alt="Event 3" height={50} width={50} /></SwiperSlide>
+            <SwiperSlide><Image src="/event4.jpg" alt="Event 4" height={50} width={50} /></SwiperSlide>
+            <SwiperSlide><Image src="/event5.jpg" alt="Event 5" height={50} width={50} /></SwiperSlide>
+            <SwiperSlide><Image src="/event1.jpg" alt="Event 1" height={50} width={50} /></SwiperSlide>
+            <SwiperSlide><Image src="/event2.jpg" alt="Event 2" height={50} width={50} /></SwiperSlide>
+            <SwiperSlide><Image src="/event3.jpg" alt="Event 3" height={50} width={50} /></SwiperSlide>
+            <SwiperSlide><Image src="/event4.jpg" alt="Event 4" height={50} width={50} /></SwiperSlide>
+            <SwiperSlide><Image src="/event5.jpg" alt="Event 5" height={50} width={50} /></SwiperSlide>
+            </Swiper>
+          </div>
+      </div>
+    
+
+      {/* --------------------Speakers---------------------- */}
+      <div id="speakers" className="speakers section-spacing">
+        <h2>Upcoming Speakers</h2>
+        <div>
+          <Image src="/ayush.png" alt="Speaker 1" height={50} width={50} />
+          <p>Speaker 1 - Discussing innovative ideas in technology.</p>
+        </div>
+        <div>
+          <p>Speaker 2 - Exploring the power of resilience in failure.</p>
+          <Image src="/abhigyan.png" alt="Speaker 2" height={50} width={50} />
+        </div>
+        <div>
+          <Image src="/ankit.jpg" alt="Speaker 3" height={50} width={50} />
+          <p>Speaker 3 - The future of artificial intelligence.</p>
+        </div>
+        <div>
+          <p>Speaker 4 - Transforming setbacks into opportunities.</p>
+          <Image src="/mayank.png" alt="Speaker 4" height={50} width={50} />
+        </div>
+        <div>
+          <Image src="/speaker5.jpg" alt="Speaker 5" height={50} width={50} />
+          <p>Speaker 5 - Leadership and personal growth.</p>
+        </div>
+        <div>
+          <p>Speaker 6 - Mental well-being and success.</p>
+          <Image src="/speaker6.jpg" alt="Speaker 6" height={50} width={50} />
         </div>
       </div>
     </>
